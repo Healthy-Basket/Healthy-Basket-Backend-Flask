6b372f19f5c417282e3448f10aa5288d83e7ac80
bernard karaba <bkaraba14@gmail.com>
2024-10-17 22:17:30 +0300

    create flask backend

---
+++ config.py
@@ -0,0 +1,33 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    DEBUG = False
+    TESTING = False
+
+class DevelopmentConfig(Config):
+    MONGO_URI = "mongodb+srv://bernard:HRlbAhKUhNd4hutY@master.rabgt.mongodb.net/HealthyBasket?retryWrites=true&w=majority&appName=master"
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    #JWT_SECRET_KEY = os.getenv("MY_JWT_SECRET")
+    #JWT_ACCESS_TOKEN_EXPIRES = False
+    
+
+class TestingConfig(Config):
+    DEBUG = True
+    TESTING = True
+
+class ProductionConfig(Config):
+    MONGO_URI = "mongodb+srv://bernard:HRlbAhKUhNd4hutY@master.rabgt.mongodb.net/?retryWrites=true&w=majority&appName=master"
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    #JWT_SECRET_KEY = os.getenv("MY_JWT_SECRET")
+    #JWT_ACCESS_TOKEN_EXPIRES = False
+   
+    
+
+config_by_name = dict(
+    dev=DevelopmentConfig,
+    test=TestingConfig,
+    prod=ProductionConfig
+    )
---
+++ onboarding.py
@@ -0,0 +1,3 @@
+from flask import Blueprint, request
+
+onboarding = Blueprint('onboarding', __name__, url_prefix='/api/v1')
