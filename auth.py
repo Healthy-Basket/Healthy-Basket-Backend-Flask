6be7175dc4d06bc9d12e37c7b354089a7a7aa345
bernard karaba <bkaraba14@gmail.com>
2024-10-19 19:12:56 +0300

    fitbit authorize

--- auth.py
+++ auth.py
@@ -13,7 +13,7 @@ authorization_uri = os.getenv("FITBIT_AUTHORIZATION_URI")
 token_url = os.getenv("TOKEN_REQUEST_URI")
 # OAuth2 session
 # Define the scopes for the OAuth2 session
-scopes = ['activity', 'nutrition', 'heartrate', 'location']
+scopes = ['activity', 'sleep', 'weight','profile', 'nutrition', 'heartrate', 'location']
 oauth = OAuth2Session(client_id, redirect_uri=redirect_uri, scope=scopes)
 
 @auth.route('/authorize')
@@ -22,7 +22,7 @@ def authorize():
     authorization_url, state = oauth.authorization_url(authorization_uri)
     session['oauth_state'] = state
     #return jsonify({"msg":state,"msg2": authorization_url})
-    return redirect(url)
+    return redirect(authorization_url)
 
 @auth.route('/callback')
 def callback():
